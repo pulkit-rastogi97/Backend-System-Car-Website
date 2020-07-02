a6ae93b410b7fba9389096dcc9644b7ffa43fc1a
--local <pulkit.rastogi97@gmail.com>
2020-07-02 19:39:16 +0530

    Pricing-Service tests working now

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,21 +1,18 @@
 package com.udacity.pricing;
 
-import com.udacity.pricing.entity.Price;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -41,8 +38,7 @@ public class PricingServiceApplicationTests {
 					.andExpect(status().isOk())
 					.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
 					.andExpect(jsonPath("currency", is("INR")))
-					.andExpect(jsonPath("price", is(56400.98)))
-					.andExpect(jsonPath("vehicleId", is(3))
+					.andExpect(jsonPath("price", is(56400.98))
 					);
 
 	}
@@ -56,8 +52,7 @@ public class PricingServiceApplicationTests {
 				   .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
 				   .andExpect(jsonPath("_embedded.prices", hasSize(10)))
 				   .andExpect(jsonPath("_embedded.prices[7].currency", is("INR")))
-				   .andExpect(jsonPath("_embedded.prices[7].price", is(90123.86)))
-				   .andExpect(jsonPath("_embedded.prices[7].vehicleId", is(8))
+				   .andExpect(jsonPath("_embedded.prices[7].price", is(90123.86))
 				   );
 
 	}
